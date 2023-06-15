af28974049543920d13071d189fc4894c341a510
jymyte <jyrimy@gmail.com>
2023-06-15 20:55:58 +0300

    Made a fresh react app called hcb and installed mui and it's dependencies on it. Example code has been stripped off as well.
    Eslint error about the changed path has been fixed.

--- hcb/README.md
+++ hcb/README.md
@@ -1,6 +1,3 @@
-Data import
-Data vas validated and filtered with Python(Pandas).
-
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ hcb/src/App.js
@@ -0,0 +1,15 @@
+import { Button, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head>
+      <Button variant="contained">Hello world!</Button>
+      <Typography>Jiihaa</Typography>
+    </div>
+  );
+}
+
+export default App;
